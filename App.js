062cd5ef6e0c3abb1ad40a6d63c7fffdf8f6a5d9
che122 <106135369+che122@users.noreply.github.com>
2022-06-02 12:30:42 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React,{useCallback} from 'react';
 import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './Home';
+import Main from "./Main.js";
+import Map from "./Map.js";
+import CalendarScreen from "./Calendar.js";
 import SignupScreen from './SignUp';
 import {
   StyleSheet,
@@ -13,6 +17,45 @@ import {
 import LoginScreen from './Login';
 const Stack = createNativeStackNavigator();
 
+const MainCalendar = createNativeStackNavigator();
+
+function MainCalendarScreen() {
+  return (
+    <MainCalendar.Navigator screenOptions={{headerShown: false}}>
+      <MainCalendar.Screen name="Main" component={Main} />
+      <MainCalendar.Screen name="Calendar" component={CalendarScreen} />
+    </MainCalendar.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+function TabScreen() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="MainScreen" screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'MainScreen') {
+              iconName = 'home-outline';
+            } else if (route.name === 'Map') {
+              iconName = "navigate-outline";
+            } else if (route.name === 'Exercise') {
+              iconName = "play-circle";
+            } else {
+              iconName = 'stats-chart-outline';
+            }
+
+            return <Ionicon name={iconName} size={size} color={color} />;
+          }}), {headerShown: false}}>
+          <Tab.Screen name = "MainScreen" component = {MainCalendarScreen}/>
+          <Tab.Screen name = "Map" component = {Map}/>
+          <Tab.Screen name = "Exercise" component = {Map}/>
+          <Tab.Screen name = "Stat" component = {Map}/>
+        </Tab.Navigator>
+      </NavigationContainer>);
+}
+
 function App() {
   return (
   <NavigationContainer>
@@ -22,6 +65,7 @@ function App() {
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="SignUp" component={SignupScreen} />
+      <Stack.Screen name="Tab" component={TabScreen} />
     </Stack.Navigator>
     </NavigationContainer>);
 }
